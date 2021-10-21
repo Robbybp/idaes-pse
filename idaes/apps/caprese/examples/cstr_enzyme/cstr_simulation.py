fedca17b10c4adebc404c4c46f2bde59b27753cc
Robert Parker <robbybparker@gmail.com>
2021-10-21 15:13:28 -0400

    partition example into smaller functions

--- idaes/apps/caprese/examples/cstr_enzyme/cstr_simulation.py
+++ idaes/apps/caprese/examples/cstr_enzyme/cstr_simulation.py
@@ -16,14 +16,15 @@ Example for Caprese's module for simulation of a plant.
 import random
 from idaes.apps.caprese.dynamic_builder import DynamicSim
 from idaes.apps.caprese.util import apply_noise_with_bounds
-from pyomo.environ import SolverFactory, Reference
+from pyomo.environ import SolverFactory, Reference, ComponentUID
 from pyomo.dae.initialization import solve_consistent_initial_conditions
 # import idaes.logger as idaeslog
-from idaes.apps.caprese.examples.cstr_model import make_model
+from idaes.apps.caprese.examples.cstr_enzyme.cstr_model import make_model
 from idaes.apps.caprese.data_manager import PlantDataManager
 from idaes.apps.caprese.plotlibrary import (
         plot_plant_state_evolution,
         plot_control_input)
+import numpy as np
 
 __author__ = "Kuan-Han Lin"
 
@@ -41,31 +42,31 @@ else:
     solver = None
 
 
-def main():
-    sample_time = 0.5
-    m_plant = make_model(horizon=sample_time, ntfe=5, ntcp=2)
-    time_plant = m_plant.fs.time
+def simulate_model(model, input_data, sample_time):
+    m_plant = model
+    input_array, inputs = input_data
+    time = model.fs.time
+    t0 = time.first()
+
+    n_samples = input_array.shape[0]
 
     # We must identify for the plant which variables are our
     # inputs and measurements.
-    inputs = [
-            m_plant.fs.mixer.S_inlet.flow_vol[0],
-            m_plant.fs.mixer.E_inlet.flow_vol[0],
-            ]
+    input_vardata = [m_plant.find_component(name)[t0] for name in inputs]
     measurements = [
-            m_plant.fs.cstr.outlet.conc_mol[0, 'C'],
-            m_plant.fs.cstr.outlet.conc_mol[0, 'E'],
-            m_plant.fs.cstr.outlet.conc_mol[0, 'S'],
-            m_plant.fs.cstr.outlet.conc_mol[0, 'P'],
-            m_plant.fs.cstr.outlet.temperature[0],
-            m_plant.fs.cstr.volume[0],
-            ]
+        m_plant.fs.cstr.outlet.conc_mol[0, 'C'],
+        m_plant.fs.cstr.outlet.conc_mol[0, 'E'],
+        m_plant.fs.cstr.outlet.conc_mol[0, 'S'],
+        m_plant.fs.cstr.outlet.conc_mol[0, 'P'],
+        m_plant.fs.cstr.outlet.temperature[0],
+        m_plant.fs.cstr.volume[0],
+    ]
 
     # Construct the "plant simulator" object
     simulator = DynamicSim(
                     plant_model=m_plant,
                     plant_time_set=m_plant.fs.time,
-                    inputs_at_t0=inputs,
+                    inputs_at_t0=input_vardata,
                     measurements_at_t0=measurements,
                     sample_time=sample_time,
                     )
@@ -74,31 +75,15 @@ def main():
 
     p_t0 = simulator.plant.time.first()
     p_ts = simulator.plant.sample_points[1]
-    #--------------------------------------------------------------------------
-    # Declare variables of interest for plotting.
-    # It's ok not declaring anything. The data manager will still save some
-    # important data.
-    states_of_interest = [Reference(simulator.plant.mod.fs.cstr.control_volume.material_holdup[:,'aq','S']),
-                          Reference(simulator.plant.mod.fs.cstr.control_volume.material_holdup[:,'aq','E']),
-                          # Reference(simulator.plant.mod.fs.cstr.control_volume.material_holdup[:,'aq','C']),
-                          # Reference(simulator.plant.mod.fs.cstr.control_volume.material_holdup[:,'aq','P']),
-                          # Reference(simulator.plant.mod.fs.cstr.control_volume.material_holdup[:,'aq','Solvent']),
-                          Reference(simulator.plant.mod.fs.cstr.control_volume.energy_holdup[:,'aq']),
-                          ]
 
     # Set up data manager to save plant data
-    data_manager = PlantDataManager(plant, states_of_interest)
+    data_manager = PlantDataManager(plant)
     #--------------------------------------------------------------------------
     solve_consistent_initial_conditions(plant, plant.time, solver)
 
-    cinput1 = [0.56, 3.48, 5.00, 0.96, 2.06,
-               5.00, 2.29, 3.91, 3.46, 5.0]
-    cinput2 = [0.29, 0.01, 0.01, 0.01, 0.13,
-               0.01, 1.00, 0.24, 0.71, 0.01]
-
     data_manager.save_initial_plant_data()
 
-    cinput = [cinput1[0], cinput2[0]]
+    cinput = input_array[0, :]
     plant.inject_inputs(cinput)
 
     # This "initialization" really simulates the plant with the new inputs.
@@ -107,12 +92,12 @@ def main():
     solver.solve(simulator.plant, tee = True)
     data_manager.save_plant_data(iteration = 0)
 
-    for i in range(1, 10):
+    for i in range(1, n_samples):
         print('\nENTERING SIMULATOR LOOP ITERATION %s\n' % i)
 
         simulator.plant.advance_one_sample()
         simulator.plant.initialize_to_initial_conditions()
-        cinput = [cinput1[i], cinput2[i]]
+        cinput = input_array[i, :]
         simulator.plant.inject_inputs(cinput)
 
         simulator.plant.initialize_by_solving_elements(solver)
@@ -120,14 +105,38 @@ def main():
         solver.solve(simulator.plant, tee = True)
         data_manager.save_plant_data(iteration = i)
 
-    plot_plant_state_evolution(states_of_interest, data_manager.plant_df)
-
-    inputs_to_plot = [Reference(simulator.plant.mod.fs.mixer.S_inlet.flow_vol[:]),
-                      # Reference(simulator.plant.mod.fs.mixer.E_inlet.flow_vol[:]),
-                      ]
-    plot_control_input(inputs_to_plot, data_manager.plant_df)
-
     return simulator, data_manager
 
+
+def plot_states_and_inputs(data_manager):
+    states_of_interest = data_manager.plant_states_of_interest
+    plot_plant_state_evolution(states_of_interest, data_manager.plant_df)
+    inputs_to_plot = data_manager.inputs
+    plot_control_input(inputs_to_plot, data_manager.plant_df)
+
+
+def main():
+    sample_time = 0.5
+    model = make_model(horizon=sample_time, ntfe=5, ntcp=2)
+
+    n_samples = 10
+    sample_points = [sample_time*(i+1) for i in range(n_samples)]
+    inputs = [
+        str(ComponentUID(model.fs.mixer.S_inlet.flow_vol[:])),
+        str(ComponentUID(model.fs.mixer.E_inlet.flow_vol[:])),
+    ]
+    input_values = [2.1, 0.1]
+    n_inputs = len(inputs)
+    input_array = np.fromiter(
+        (input_values[i] for t in sample_points for i in range(n_inputs)),
+        float,
+    )
+    input_array = input_array.reshape((n_samples, n_inputs))
+    input_data = (input_array, inputs)
+
+    simulator, data_manager = simulate_model(model, input_data, sample_time)
+    plot_states_and_inputs(data_manager)
+
+
 if __name__ == '__main__':
-    simulator, data_manager = main()
+    main()
